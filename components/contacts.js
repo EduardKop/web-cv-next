0a68d322af8faa5970418ad9942d8294ef05b5f0
Eduard <eduard.korytnyk@gmail.com>
2022-12-05 17:48:08 +0200

    add flexible layout

--- components/contacts.js
+++ components/contacts.js
@@ -3,11 +3,12 @@ import React from 'react';
 export default function Contacts() {
 return (
     <div className="contacts">
+    
             <div className="contacts-container" >
 
     <div className='contacts-wrap'><span>Contacts</span></div>
         <div className='contacts-item first '>
-        <a rel="shortcut icon noopener noreferrer"target="_blank" href={'+380665249893'} > 📞 {'tel:+380665249893'}</a>
+        <a rel="shortcut icon noopener noreferrer"target="_blank" href={'tel:+380665249893'} > 📞 {'+380665249893'}</a>
         </div>
         <div className='contacts-item'>
         <a rel="shortcut icon noopener noreferrer" target="_blank" href={'https://t.me/Eduard_Kop'}> 📱 {'t.me/Eduard_Kop'}</a>
@@ -16,7 +17,7 @@ return (
         <a rel="shortcut icon noopener noreferrer" target="_blank"  href={'https://www.linkedin.com/in/eduard-korytnyk-6b3929155/'}>🌐 {"LinkedIn"}</a>
         </div>
         <div className='contacts-item'>
-        <a rel="shortcut icon noopener noreferrer" target="_blank" href={'mailto:eduard.korytnyk@gmail.com'}> 📭 {'eduard.korytnyk@gmail.com'}</a>
+        <a rel="shortcut icon noopener noreferrer" href={'mailto:eduard.korytnyk@gmail.com'}> 📭 {'eduard.korytnyk@gmail.com'}</a>
         </div>
     </div>
     </div>
