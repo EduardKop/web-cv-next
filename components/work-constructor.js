3500c5d4829185b4fb99e08b7c897ca47971eafb
Eduard <eduard.korytnyk@gmail.com>
2023-06-06 00:08:09 +0300

    optimized the image

--- components/work-constructor.js
+++ components/work-constructor.js
@@ -1,42 +1,46 @@
 import React from "react";
 import Image from 'next/image'
 
+function WorkConstructor ({name, stack, img, link, linkName, pageHref, webName, PageLinkName, pageName}) {
+  const technologies = stack.split(', ');
 
-function WorkConstructor ({name,stack,img,link,linkName,pageHref,webName,PageLinkName,pageName}) {
-    const technologies = stack.split(', '); // Split the stack string into an array of technologies
-    return (
-        <div className='expirience-container'>
-            <div className='expirience-container__item'>
-            <div className="expirience-container-img">
-            <Image
+  return (
+    <div className='expirience-container'>
+      <div className='expirience-container__item'>
+        <div className="expirience-container-img">
+          <Image
             src={img}
             alt="Picture of the author"
-            width='250px'
-            height='140px'
-            />
-            </div>
-            <div className="expirience-container-img--discription">
-            <div className='container__item-nameContainer'>
-            {name} 
-                </div> 
-            <div className="links">
-            <div className="container__item-linkContainer">
-           <span>Github:</span><a rel="shortcut icon noreferrer" href={link} target="_blank">{linkName}</a>
-                </div>
-            <div className="container__item-webContainer">
-            <span>{pageName}</span> <br></br><a rel="shortcut icon noreferrer" href={pageHref} target="_blank">{PageLinkName}</a>
-                </div>   
-            </div>
-            
-            <div className='container__item-stackContainer'>
-            {technologies.map((tech, index) => (
-  <div key={index} className={`card card-${tech}`}>{tech}</div>
-))}
-            </div>
-            </div>
-            </div>
-
+            width={250}
+            height={140}
+          />
         </div>
-    )
+        <div className="expirience-container-img--discription">
+          <div className='container__item-nameContainer'>
+            {name} 
+          </div> 
+          <div className="links">
+            <div className="container__item-linkContainer">
+              <span>Github:</span>
+              <a rel="shortcut icon noreferrer" href={link} target="_blank">{linkName}</a>
+            </div>
+            {pageHref && (
+              <div className="container__item-webContainer">
+                <span>{pageName}</span> <br />
+                <a rel="shortcut icon noreferrer" href={pageHref} target="_blank">{PageLinkName}</a>
+              </div>
+            )}
+          </div>
+            
+          <div className='container__item-stackContainer'>
+            {technologies.map((tech, index) => (
+              <div key={index} className={`card card-${tech}`}>{tech}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default WorkConstructor;
